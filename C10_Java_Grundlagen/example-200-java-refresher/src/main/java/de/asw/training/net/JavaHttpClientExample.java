a4ab57ead9689f031d995d010f2e0ed1fcde52a9
Thomas Darimont <thomas.darimont@googlemail.com>
2020-11-10 20:22:10 +0100

    Add simple Java http client usage example

--- C10_Java_Grundlagen/example-200-java-refresher/src/main/java/de/asw/training/net/JavaHttpClientExample.java
+++ C10_Java_Grundlagen/example-200-java-refresher/src/main/java/de/asw/training/net/JavaHttpClientExample.java
@@ -9,7 +9,42 @@ import java.net.http.HttpResponse;
 
 public class JavaHttpClientExample {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
+
+        simpleGetRequestDemo();
+
+        simplePostRequestDemo();
+    }
+
+    private static void simplePostRequestDemo() throws Exception {
+
+        System.out.println("#### simplePostRequestDemo");
+
+        var body = HttpRequest.BodyPublishers.ofString("test123");
+
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create("https://httpbin.org/post"))
+                .header("Content-type", "text/plain")
+                .POST(body)
+                .build();
+
+        var client = HttpClient.newHttpClient();
+
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        var statusCode = response.statusCode();
+        System.out.printf("### Status Code: %s%n", statusCode);
+
+        HttpHeaders headers = response.headers();
+        System.out.printf("### Response Headers: %s%n", headers);
+
+        System.out.println("### Body:");
+        System.out.println(response.body());
+    }
+
+    private static void simpleGetRequestDemo() throws Exception {
+
+        System.out.println("#### simpleGetRequestDemo");
 
         var request = HttpRequest.newBuilder()
                 .uri(URI.create("https://example.com"))
