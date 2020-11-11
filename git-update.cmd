f5304879ab58df19f42d132793522096b76ae9ff
Thomas Darimont <thomas.darimont@googlemail.com>
2020-11-11 21:56:11 +0100

    Add examples update scripts

---
+++ git-update.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+echo "Updating Project Files..."
+
+# add current changes to the git index
+git add .
+
+# Stash local changes away
+git stash save
+
+# Pull new code
+git pull
+
+echo "Updating Project Files completed."
