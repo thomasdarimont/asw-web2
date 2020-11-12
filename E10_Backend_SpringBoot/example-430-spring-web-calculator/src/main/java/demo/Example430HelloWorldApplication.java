95d98fd32ac185ecc8e1ad1b09b6e48fe66fed73
Thomas Darimont <thomas.darimont@googlemail.com>
2020-11-12 20:25:51 +0100

    Add reference to Url to Example430HelloWorldApplication

--- E10_Backend_SpringBoot/example-430-spring-web-calculator/src/main/java/demo/Example430HelloWorldApplication.java
+++ E10_Backend_SpringBoot/example-430-spring-web-calculator/src/main/java/demo/Example430HelloWorldApplication.java
@@ -3,6 +3,9 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Den Taschenrechner kann man unter der folgenden URL aufrufen: http://localhost:8080
+ */
 @SpringBootApplication
 public class Example430HelloWorldApplication {
 
