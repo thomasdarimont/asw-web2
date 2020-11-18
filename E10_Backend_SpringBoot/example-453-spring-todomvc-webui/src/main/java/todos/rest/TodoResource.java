8738b649d77a2616184a572df7be07b26c63f611
Thomas Darimont <thomas.darimont@googlemail.com>
2020-11-18 12:29:01 +0100

    Add searchWithParams to TodoResource in example-453

--- E10_Backend_SpringBoot/example-453-spring-todomvc-webui/src/main/java/todos/rest/TodoResource.java
+++ E10_Backend_SpringBoot/example-453-spring-todomvc-webui/src/main/java/todos/rest/TodoResource.java
@@ -2,15 +2,7 @@ package todos.rest;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import todos.Todo;
 import todos.TodoService;
@@ -112,6 +104,14 @@ public class TodoResource {
   public ResponseEntity<List<?>> search(@RequestBody Todo example) {
 
     List<?> result = this.todos.findAllByExample(example);
+
+    return ResponseEntity.ok(result);
+  }
+
+  @GetMapping("/search")
+  public ResponseEntity<?> searchWithParams(Todo example) {
+
+    List<?> result = todos.findAllByExample(example);
 
     return ResponseEntity.ok(result);
   }
