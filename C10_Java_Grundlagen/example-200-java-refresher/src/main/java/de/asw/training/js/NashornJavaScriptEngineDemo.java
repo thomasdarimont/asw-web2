aadc50cf9e051a00dec130ff26ab32ced11af16f
Thomas Darimont <thomas.darimont@googlemail.com>
2020-11-27 10:42:16 +0100

    Add note about running nashorn example

--- C10_Java_Grundlagen/example-200-java-refresher/src/main/java/de/asw/training/js/NashornJavaScriptEngineDemo.java
+++ C10_Java_Grundlagen/example-200-java-refresher/src/main/java/de/asw/training/js/NashornJavaScriptEngineDemo.java
@@ -5,6 +5,16 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+/**
+ * This only works with Java Version < 15
+ *
+ *
+ * cd c:\asw\asw-web2\C10_Java_Grundlagen\example-200-java-refresher
+ *
+ * "C:\Program Files\OpenJDK\jdk-11"\bin\java -cp target\classes de.asw.training.js.NashornJavaScriptEngineDemo
+ * Warning: Nashorn engine is planned to be removed from a future JDK release
+ * Result: 33
+ */
 class NashornJavaScriptEngineDemo {
 
     public static void main(String[] args) throws ScriptException {
