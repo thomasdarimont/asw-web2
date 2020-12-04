52c61471ba18f0183c4f41aa5eba1fbe143643db
Thomas Darimont <thomas.darimont@googlemail.com>
2020-12-04 08:55:36 +0100

    Fix typo in example

--- L10_Labs/lab-375-javaee-servlet-http-sessions/src/main/java/lab/CustomHttpSessionListener.java
+++ L10_Labs/lab-375-javaee-servlet-http-sessions/src/main/java/lab/CustomHttpSessionListener.java
@@ -5,18 +5,24 @@ import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 // @??? // YOURCODE use the proper Annotation for Session Listeners
-public class CustomHttpSessionListener 
-	// implements ??? { // YOURCODE use the proper interface for Http Session listeners
-	{ // remove this line
+public class CustomHttpSessionListener
+        // implements ??? { // YOURCODE use the proper interface for Http Session listeners
+{ // remove this line
 
-	public void sessionCreated(HttpSessionEvent se) {
-		// access the session id from the HttpSessionEvent
-		System.out.println("Session erzeugt ID:" + se.get???().get???());
-	}
+    public void sessionCreated(HttpSessionEvent se) {
+        // access the session id from the HttpSessionEvent
+        System.out.println("Session erzeugt ID:" +
+                //			se.get???().get???()  // YOURCODE
+                null // REMOVE this line
+        );
+    }
 
-	public void sessionDestroyed(HttpSessionEvent se) {
-		// access the session id from the HttpSessionEvent
-		System.out.println("Session zerstört ID:" + se.get???().get???());
-	}
+    public void sessionDestroyed(HttpSessionEvent se) {
+        // access the session id from the HttpSessionEvent
+        System.out.println("Session zerstört ID:" +
+                // se.get???().get???()   // YOURCODE
+                null // REMOVE this line
+        );
+    }
 
 }
