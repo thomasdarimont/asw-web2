95b69f7cf04b01a76b4c47654da5aa958e7c4603
Thomas Darimont <thomas.darimont@googlemail.com>
2020-12-08 19:46:16 +0100

    Revise DI example

--- E10_Backend_SpringBoot/example-403-spring-dependency-injection-with-scanning/src/main/java/demo/shop/product/ProductService.java
+++ E10_Backend_SpringBoot/example-403-spring-dependency-injection-with-scanning/src/main/java/demo/shop/product/ProductService.java
@@ -1,25 +1,27 @@
 package demo.shop.product;
 
-import demo.shop.catalog.CatalogService;
-import demo.shop.inventory.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import demo.shop.catalog.CatalogService;
+import demo.shop.inventory.InventoryService;
 
 /**
  * This class is found via "Component-Scanning" and registered as a Spring Bean
  * Component.
  */
-@Component // Note: a more specialized version of @Component is @Service which implies
-// additional semantics
+// Note: a more specialised version of @Component is @Service which implies
+//additional semantics
+@Component
 public class ProductService {
 
     private final CatalogService catalog;
 
     private final InventoryService inventory;
 
-    @Autowired // Note: just for demonstration purposes, @Autowired not needed here as the
-    // latest Spring versions automatically detect this constructor as
-    // Injection-Point!
+    //	Note, this is just for demonstration purposes: @Autowired is not needed here as the
+    // latest Spring versions automatically detect this constructor as Injection-Point!
+    @Autowired 
     public ProductService(CatalogService catalog, InventoryService inventory) {
         this.catalog = catalog;
         this.inventory = inventory;
